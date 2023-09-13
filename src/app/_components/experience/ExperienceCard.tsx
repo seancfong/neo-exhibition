daa4f36746b87db307275e0eca1285a855fdfc97
Sean Fong <seancfong@gmail.com>
2023-09-12 18:28:09 -0700

    improvement: Made experience font smaller on large screen to accommodate for navbar

--- src/app/_components/experience/ExperienceCard.tsx
+++ src/app/_components/experience/ExperienceCard.tsx
@@ -21,11 +21,11 @@ const ExperienceCard = ({ experienceData }: ExperienceCardType) => {
   return (
     <>
       {/* Mobile Card */}
-      <div className="relative z-10 grid h-fit max-h-[80vh] min-w-[330px] max-w-[min(90vw,600px)] flex-1 grid-cols-5 grid-rows-[auto_auto_1fr] lg:max-w-3xl lg:overflow-y-auto 2xl:max-w-4xl">
+      <div className="relative z-10 grid h-fit max-h-[80vh] min-w-[330px] max-w-[min(90vw,600px)] flex-1 grid-cols-5 grid-rows-[auto_auto_1fr] lg:max-w-3xl lg:overflow-y-auto">
         {/* Start & End Date (r1, c1) */}
         <div className="col-start-1 row-start-1 flex h-fit w-fit justify-end gap-2 justify-self-end overflow-x-visible pb-3 pr-2 pt-10">
           {/* Date Container */}
-          <div className="relative h-fit whitespace-nowrap pt-5 text-right text-sm text-dim sm:text-base lg:text-lg 2xl:text-2xl">
+          <div className="relative h-fit whitespace-nowrap pt-5 text-right text-sm text-dim sm:text-base lg:text-lg">
             <p>{startDate}</p>
             <p className="pr-1">|</p>
             <p>{endDate}</p>
@@ -34,7 +34,7 @@ const ExperienceCard = ({ experienceData }: ExperienceCardType) => {
           <div className="h-auto w-0.5 rounded-full bg-cyellow opacity-50" />
         </div>
         {/* Content Card (r1, c2) */}
-        <div className="relative col-start-2 col-end-6 row-start-1 row-end-3 flex flex-col gap-3 p-5 lg:gap-4 lg:px-10 lg:py-6 2xl:py-12">
+        <div className="relative col-start-2 col-end-6 row-start-1 row-end-3 flex flex-col gap-3 p-5 lg:gap-4 lg:px-10 lg:py-6">
           {/* Logo */}
           <div className="block lg:hidden">
             <Image
@@ -64,7 +64,7 @@ const ExperienceCard = ({ experienceData }: ExperienceCardType) => {
             />
             <div className="absolute left-0 top-0 -z-10 h-full w-full rounded-lg rounded-br-none bg-gradient-to-r from-[rgba(41,41,41,0.8)] to-[rgba(91,84,84,0.3)] backdrop-blur-[2px]" />
           </div>
-          <h4 className="text-3xl font-light leading-8 tracking-wide sm:text-4xl lg:text-5xl 2xl:text-6xl">
+          <h4 className="text-3xl font-light leading-8 tracking-wide sm:text-4xl lg:text-5xl">
             {position}
           </h4>
           <p className="text-sm font-light tracking-wide sm:text-base lg:text-2xl">
@@ -75,7 +75,7 @@ const ExperienceCard = ({ experienceData }: ExperienceCardType) => {
         <div className="row-span-2 row-start-2 w-5 justify-self-end rounded-l-lg bg-panel-light lg:w-10" />
         {/* Bullets & Skills (r3, c2) */}
         <div className="col-start-2 col-end-6 row-start-3 rounded-br-lg bg-panel-darkest p-5 pt-3">
-          <ul className="list-inside font-light tracking-wider sm:text-lg lg:text-xl 2xl:text-3xl">
+          <ul className="list-inside font-light tracking-wider sm:text-lg lg:text-2xl">
             {bullets.map((bullet) => {
               const { _key, content } = bullet;
               return (
@@ -88,7 +88,7 @@ const ExperienceCard = ({ experienceData }: ExperienceCardType) => {
               );
             })}
           </ul>
-          <ul className="flex flex-wrap gap-x-3 gap-y-2 text-xs font-light uppercase tracking-widest text-dim sm:text-sm lg:text-base 2xl:text-lg">
+          <ul className="flex flex-wrap gap-x-3 gap-y-2 text-xs font-light uppercase tracking-widest text-dim sm:text-sm lg:text-base">
             {skills?.map((skill, i) => {
               const { _id, name } = skill;
               return (
