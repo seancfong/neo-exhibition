0f850f1c080cba85d217cfe21a4c58666cc6f404
Sean Fong <seancfong@gmail.com>
2023-09-17 00:59:30 -0700

    improvement: Added twitter card markup for large preview image

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -24,6 +24,18 @@ export const metadata: Metadata = {
       },
     ],
   },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@sean_cnfg",
+    images: [
+      {
+        url: "https://seancfong.com/images/og.jpg",
+        width: "1200",
+        height: "630",
+        type: "image/jpg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
